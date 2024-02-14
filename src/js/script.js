763ccc9c0b2de98e71ded3ceb5b6d21daf59b5d2
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2024-02-14 21:10:23 +0900

    20230214

--- src/js/script.js
+++ src/js/script.js
@@ -38,18 +38,18 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
     // Fvスライダー
-    const mv_swiper = new Swiper(".js-fv-swiper", {
-        loop: true,
-        speed: 2000,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    });
+    // const mv_swiper = new Swiper(".js-fv-swiper", {
+    //     loop: true,
+    //     speed: 2000,
+    //     effect: "fade",
+    //     fadeEffect: {
+    //         crossFade: true,
+    //     },
+    //     autoplay: {
+    //         delay: 4000,
+    //         disableOnInteraction: false,
+    //     },
+    // });
 
 
     // キャンペーンスライダー（swiper）
