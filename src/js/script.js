87c1f98688e196889ae84d24d640ba67a73c0191
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2024-02-15 13:13:09 +0900

    20240215

--- src/js/script.js
+++ src/js/script.js
@@ -71,43 +71,11 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
                 spaceBetween: 40, // スライド間の余白（px）
             }
         },
-        // pagination: {
-        //     el: ".swiper-pagination", //paginationのclass
-        //     clickable: true, //クリックでの切り替えを有効に
-        //     //type: "progressbar" //paginationのタイプ (※2)
-        // },
         navigation: {
             prevEl: ".swiper-button-prev", //戻るボタンのclass
             nextEl: ".swiper-button-next", //進むボタンのclass
         },
-        // scrollbar: { //スクロールバーを表示したいとき
-        //     el: ".swiper-scrollbar", //スクロールバーのclass
-        //     hide: true, //操作時のときのみ表示
-        //     draggable: true //スクロールバーを直接表示できるようにする
-        // },
     });
-    /* =================================================== 
-※1 effectについて
-slide：左から次のスライドが流れてくる
-fade：次のスライドがふわっと表示
-■ fadeの場合は下記を記述
-    fadeEffect: {
-        crossFade: true
-    },
-cube：スライドが立方体になり、3D回転を繰り返す
-coverflow：写真やアルバムジャケットをめくるようなアニメーション
-flip：平面が回転するようなアニメーション
-cards：カードを順番にみていくようなアニメーション
-creative：カスタマイズしたアニメーションを使うときに使用します
-
-=======================================================
-※2 paginationのタイプ
-bullet：スライド枚数と同じ数のドットが表示
-fraction：分数で表示（例：1 / 3）
-progressbar：スライドの進捗に応じてプログレスバーが伸びる
-custom：自由にカスタマイズ
-
-=====================================================*/
 
 
 
@@ -178,6 +146,8 @@ custom：自由にカスタマイズ
         });
     });
 
+
+    // 背景色の後に画像が表示されるアニメーション
     $(function () {
         //要素の取得とスピードの設定
         var box = $('.colorbox'),
