dd4c9658e516c7ba5f7c4cf792b62c4d4bfcda8c
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2024-04-22 22:45:53 +0900

    20240422

--- src/js/script.js
+++ src/js/script.js
@@ -54,10 +54,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         const mySwiper = new Swiper('.js-campaign-swiper', { //名前を変える
             loop: true,
             loopAdditionalSlides: 1,
-            autoplay: {
-                delay: 2000,
-                disableOnInteraction: false,
-            },
+            // autoplay: {
+            //     delay: 2000,
+            //     disableOnInteraction: false,
+            // },
             speed: 2000,          //追記
             slidesPerView: "auto",
             spaceBetween: 24, // スライド間の余白（px）
@@ -137,6 +137,39 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             });
         });
 
+    });
+
+//タブきりかえ
+$(function () {
+    // 変数を要素をセット
+    var $filter = $('.page-campaign__filter-list [data-filter]'),
+        $item = $('.page-campaign__filter-items [data-item]');
+        
+    // カテゴリをクリックしたら
+    $filter.click(function (e) {
+        // デフォルトの動作をキャンセル
+        e.preventDefault();
+        var $this = $(this);
+        
+        // クリックしたカテゴリにクラスを付与
+        $filter.removeClass('is-active');
+        $this.addClass('is-active');
+        
+        // クリックした要素のdata属性を取得
+        var $filterItem = $this.attr('data-filter');
+        
+        // データ属性が all なら全ての要素を表示
+        if ($filterItem == 'all') {
+        $item.removeClass('is-active').fadeOut().promise().done(function () {
+            $item.addClass('is-active').fadeIn();
+        });
+        // all 以外の場合は、クリックした要素のdata属性の値を同じ値のアイテムを表示
+        } else {
+        $item.removeClass('is-active').fadeOut().promise().done(function () {
+            $item.filter('[data-item = "' + $filterItem + '"]').addClass('is-active').fadeIn();
+        });
+        }
+    });
     });
 
 
